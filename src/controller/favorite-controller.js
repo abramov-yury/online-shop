06132e735e270d4c1ff8860a3e23c7e7ed67865f
Yury <yury.message@gmail.com>
2022-06-19 12:01:14 +0400

    Синхронизировать состояние иконок избранного
    Реализовать хранение избранных объявлений в localStorage

--- src/controller/favorite-controller.js
+++ src/controller/favorite-controller.js
@@ -2,6 +2,8 @@ import { render } from "../helpers/render";
 
 import { FavoriteView } from "../view/favorite-view";
 
+export const FAVORITES = "favorites";
+
 export class FavoriteController {
   constructor(container, parameters, position) {
     this.container = container;
@@ -16,6 +18,7 @@ export class FavoriteController {
   initiate() {
     this.view = new FavoriteView(this.parameters);
     this._setHandlers();
+    this._initiateToggle();
     render(this.container, this.view, this.position);
   }
 
@@ -28,8 +31,42 @@ export class FavoriteController {
     this.view.setFavoriteCheckboxChangeHandler(this.onFavoriteCheckboxChange);
   }
 
+  _initiateToggle() {
+    if(!localStorage.getItem(FAVORITES)) return;
+    if(!JSON.parse(localStorage.getItem(FAVORITES)).find(item => item === this.parameters.id)) return;
+
+    this.view.getFavoriteCheckbox().checked = "true";
+  }
+
+  _checkToggles(evt) {
+    const cbs = document.querySelectorAll(`input[name=${evt.target.name}]`);
+
+    cbs.forEach(item => {
+      if(item.checked !== evt.target.checked) item.checked = evt.target.checked;
+    });
+  }
+
+  _shakeLocalStorage() {
+    if(!localStorage.getItem(FAVORITES)) {
+      localStorage.setItem(FAVORITES, JSON.stringify([this.parameters.id]));
+      return;
+    }
+
+    let favorites = JSON.parse(localStorage.getItem(FAVORITES));
+
+    if(favorites.find(item => item === this.parameters.id)) {
+      favorites = favorites.filter(item => item !== this.parameters.id);
+      localStorage.setItem(FAVORITES, JSON.stringify(favorites));
+      return;
+    }
+
+    favorites.push(this.parameters.id);
+    localStorage.setItem(FAVORITES, JSON.stringify(favorites));
+  }
+
   onFavoriteCheckboxChange(evt) {
-    console.log(evt.target.name);
+    this._shakeLocalStorage();
+    this._checkToggles(evt);
   }
 
 }

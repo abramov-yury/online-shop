9a11da927f395038be57c09a50469b386353ac6d
yury <yury.message@gmail.com>
2022-06-21 16:22:51 +0400

    (app) Исправить некорректное расположение бока

--- src/pages/index/script.js
+++ src/pages/index/script.js
@@ -6,8 +6,8 @@ import { AppController } from "../../controller/app-controller";
 const url = "https://green-json-server.herokuapp.com/online-shop";
 
 const line = document.querySelector(".line");
-const preloaderController = new PreloaderController(line, {parent: "page"}, RenderPosition.AFTER);
-const appController = new AppController(document.body, {parent: "page"});
+const preloaderController = new PreloaderController(document.body, {parent: "page"});
+const appController = new AppController(line, {parent: "page"}, RenderPosition.AFTER);
 
 preloaderController.initiate();
 appController.initiate(url).catch(err => console.log(err));

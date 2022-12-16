2c9be46bfcab10193233fed7021a85fc5236eece
yury <yury.message@gmail.com>
2022-12-16 19:22:49 +0400

    add resources folder

--- src/pages/index/script.js
+++ src/pages/index/script.js
@@ -4,8 +4,6 @@ import { PreloaderController } from "../../controller/preloader-controller";
 import { AppController } from "../../controller/app-controller";
 
 const url = "https://api.jsonbin.io/v3/b/639b205915ab31599e1d02f8";
-const oldUrl = "https://green-json-server.herokuapp.com/online-shop";
-
 
 const line = document.querySelector(".line");
 const preloaderController = new PreloaderController(document.body, {parent: "page"});
